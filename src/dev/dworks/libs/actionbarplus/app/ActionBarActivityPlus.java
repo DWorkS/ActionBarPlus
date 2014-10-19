01d30a168388c74e11b97387cb55659c1ebe6fd4
Hari Krishna Duipudi <heart.break.kid.b4u@gmail.com>
2014-10-19 15:19:15 +0530

    removing customization for overflow menu

--- src/dev/dworks/libs/actionbarplus/app/ActionBarActivityPlus.java
+++ src/dev/dworks/libs/actionbarplus/app/ActionBarActivityPlus.java
@@ -2,13 +2,14 @@ package dev.dworks.libs.actionbarplus.app;
 
 import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.internal.view.ActionBarPolicy;
 import android.view.KeyEvent;
 
 public class ActionBarActivityPlus extends ActionBarActivity {
 
 	@Override
 	public boolean onKeyUp(int keyCode, KeyEvent event) {
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+        if (ActionBarPolicy.get(this).showsOverflowMenuButton() && Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
 			if (event.getAction() == KeyEvent.ACTION_UP
 					&& keyCode == KeyEvent.KEYCODE_MENU) {
 				openOptionsMenu();
